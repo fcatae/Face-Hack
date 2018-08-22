f596ee6a09e247f0cd7f3d80a0816c382bd6696f
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-08-22 18:50:26 -0300

    going on

--- back/server.py
+++ back/server.py
@@ -5,25 +5,22 @@ app = Flask(__name__)
 
 def get_face_id():
     data = {'url' : 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=350'}
-    headers = {'Content-Type' : 'application/json', 'Ocp-Apim-Subscription-Key' : ''}
-    
-    r = requests.post('https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/detect', json=data, headers=headers)
-    json_response_id = r.json()[0]['faceId']
-    return json_response_id
-    
-
+    response = call('https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/detect', data)
+    print(response)
+ 
 def submit_face_id(face_id):
     data = {
         "largePersonGroupId": "bancodobrasil",
-        "faceIds": [
-            "11515864-b0a2-4034-a77f-559c9b481682"
-        ]
+        "faceIds": [face_id]
     }
 
+    response = call('https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/identify', data)
+    print(response)
+
+def call(endpoint, data):
     headers = {'Content-Type' : 'application/json', 'Ocp-Apim-Subscription-Key' : ''}
-    
-    r = requests.post('https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/identify', json=data, headers=headers)
-    print(r.json())
+    r = requests.post(endpoint, json=data, headers=headers)
+    return r.json()
 
 @app.route('/')
 def hello_world():
@@ -39,5 +36,5 @@ def person_search():
     submit_face_id()
 
 if __name__ == '__main__':
-    submit_face_id('asasdasd')
+    get_face_id()
     app.run('0.0.0.0', port=5000)
