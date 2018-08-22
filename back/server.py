a26dabd5fdbe32a109f3a8aa6c534f3fd21e6808
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-08-22 16:33:03 -0300

    adding some starting lines of code

--- back/server.py
+++ back/server.py
@@ -1,9 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+def get_face_id():
+    pass
+
+def submit_face_id(face_id):
+    pass
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!\n'
+
+@app.route('/healthz')
+def health_check():
+    return 'Everything working!\n'
+
+@app.route('/api/person/search', methods=['POST'])
+def person_search():
+    face_id = get_face_id
+    submit_face_id()
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
---
+++ back/setup_api.py
@@ -0,0 +1,1 @@
+# this file will get the api ready
