b043c8d6365846d5450987038b566a68b977df9b
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-08-22 18:13:33 -0300

    going on

--- back/server.py
+++ back/server.py
@@ -1,11 +1,29 @@
+import requests
 from flask import Flask
+
 app = Flask(__name__)
 
 def get_face_id():
-    pass
+    data = {'url' : 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=350'}
+    headers = {'Content-Type' : 'application/json', 'Ocp-Apim-Subscription-Key' : ''}
+    
+    r = requests.post('https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/detect', json=data, headers=headers)
+    json_response_id = r.json()[0]['faceId']
+    return json_response_id
+    
 
 def submit_face_id(face_id):
-    pass
+    data = {
+        "largePersonGroupId": "bancodobrasil",
+        "faceIds": [
+            "11515864-b0a2-4034-a77f-559c9b481682"
+        ]
+    }
+
+    headers = {'Content-Type' : 'application/json', 'Ocp-Apim-Subscription-Key' : ''}
+    
+    r = requests.post('https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/identify', json=data, headers=headers)
+    print(r.json())
 
 @app.route('/')
 def hello_world():
@@ -21,4 +39,5 @@ def person_search():
     submit_face_id()
 
 if __name__ == '__main__':
+    submit_face_id('asasdasd')
     app.run('0.0.0.0', port=5000)
