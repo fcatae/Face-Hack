820f270857f1eddb2ca6e5ee6e3e20ed3b2b53a6
Gustavo Lima <gclima1@latam.stefanini.com>
2018-08-22 18:24:32 -0300

    Front skeleton done

--- front/static/main.js
+++ front/static/main.js
@@ -1,3 +1,6 @@
+
+
+
 function previewFile() {
     var preview = document.querySelector('img');
     var file = document.querySelector('input[type=file]').files[0];
@@ -5,6 +8,14 @@ function previewFile() {
 
     reader.addEventListener("load", function () {
         preview.src = reader.result;
+        base64 = preview.src
+        axios.get('https://reqres.in/api/users/2')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+
+        console.log('Preview', base64.split(',')[1])
     }, false);
 
     if (file) {
